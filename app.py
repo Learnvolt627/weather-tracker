b45b71e0c0d3abde8b767632154c35cda28fd096
Yashvardhan Singh Rathore <yashvardhan.rathore.ug24@nsut.ac.in>
2025-07-08 11:43:21 +0530

    enhanced api key security

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-api_key='ceeaeeaeb5b4db689ee714336c6ad8a7'
+
 
 import requests
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+api_key=os.getenv("API_KEY")
+if not api_key:
+     print("Error:API key not found . Please set API_KEY in .env file")
+     exit()
+
+
 user_input=input('Enter city:')
 
 weather_data=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
-if weather_data.json()['cod'] == 404:
+weather_json=weather_data.json()
+if weather_json['cod'] == 404:
     print('No city found')
 else:
-     weather =weather_data.json()['weather'][0]["description"]
-     temp=weather_data.json()['main']['temp']
-     feels_like=weather_data.json()['main']['feels_like']
+     weather =weather_json['weather'][0]["description"]
+     temp=weather_json['main']['temp']
+     feels_like=weather_json['main']['feels_like']
 
      print(f'The weather in {user_input} is {weather}')
      print(f'The temperature in {user_input} is {temp} Celsius')
      print(f'The weather feels like {feels_like}')
-     print(weather_data.json())
+    
            
